1e2d031028c258d99a40e5cbf34a9ce44eca69df
daw2-fal25 <falyehor@fpllefia.com>
2025-02-20 19:10:39 +0000

    commit1

---
+++ agen-bootstrap-main/theme/bbdd/config.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'mysql-yehor.alwaysdata.net';
+$dbname = 'yehor_uf3';
+$username = 'yehor';
+$password = '1407831h';
+
+$mysqli = new mysqli($host, $username, $password, $dbname);
+
+if ($mysqli->connect_error) {
+    die("Error de conexión: " . $mysqli->connect_error);
+} else {
+    echo 'conectado !!';
+}
+
+$mysqli->set_charset("utf8mb4");
+?>
---
+++ agen-bootstrap-main/theme/bbdd/index.php
@@ -0,0 +1,33 @@
+<?php
+require_once "config.php";
+
+$result = $mysqli->query("SELECT * FROM USERS WHERE name LIKE '%a%'");
+
+
+print_r($result);
+
+$result2 = $result->fetch_all(MYSQLI_ASSOC);
+
+
+print_r($result2);
+
+
+foreach($result2 as $r){
+    echo '<br/><br/>';
+    echo $r['name'];
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
